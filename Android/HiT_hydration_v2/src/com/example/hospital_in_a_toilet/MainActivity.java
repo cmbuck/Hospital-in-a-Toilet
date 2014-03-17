3e5819c9b91d214e38ff684cd90d894da5d85239
edougal <edougal@gmail.com>
2014-03-16 19:16:15 -0500

    added "receive from server" method

--- Android/HiT_hydration_v2/src/com/example/hospital_in_a_toilet/MainActivity.java
+++ Android/HiT_hydration_v2/src/com/example/hospital_in_a_toilet/MainActivity.java
@@ -1,26 +1,50 @@
 package com.example.hospital_in_a_toilet;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Locale;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.example.hospital_in_a_toilet.RGB_Wrapper;
 
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.provider.Settings.Secure;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity implements
-		ActionBar.TabListener {
+ActionBar.TabListener {
+
+    private String base_url = "http://ec2-54-237-70-7.compute-1.amazonaws.com/";
+
+    private SeekBar seekBar;
+
+    private ArrayList<RGB_Wrapper> user_history;
 
 	/**
 	 * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -171,4 +195,94 @@ public class MainActivity extends FragmentActivity implements
 		}
 	}
 
+
+// 
+
+	public void receive_from_server()
+	{
+		String android_id = Secure.getString(getBaseContext().getContentResolver(),
+                Secure.ANDROID_ID); 
+		String request = base_url + "get_data.php?username=" + android_id +
+				"&number=5";
+		connect_to_server(request,true);
+	}
+
+	public void connect_to_server(final String request, final boolean toAdd) {
+		Thread trd = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				
+				HttpClient httpClient = new DefaultHttpClient();
+				HttpGet httpGet = new HttpGet(request);
+
+				// Making HTTP Request
+				try {
+					HttpResponse response = httpClient.execute(httpGet);
+
+					// writing response to log
+					InputStream in = response.getEntity().getContent();
+					BufferedReader reader = new BufferedReader(
+							new InputStreamReader(in));
+					StringBuilder str = new StringBuilder();
+					String line = null;
+					while ((line = reader.readLine()) != null) {
+						str.append(line);
+					}
+					in.close();
+
+					Log.d("HTML", str.toString());
+					if(toAdd){
+					try {
+						final JSONObject json = new JSONObject(str.toString());
+						
+						for(Integer i=0;i<json.length();i++)
+						{
+							String res = json.getString(i.toString());
+							final JSONObject json_2 = new JSONObject(res);
+							String R = json_2.getString("R");
+							String G = json_2.getString("G");
+							String B = json_2.getString("B");
+							String SENT_TIME = json_2.getString("SENT_TIME");
+							Log.e("JSON", R + G + B +SENT_TIME);
+							RGB_Wrapper rgb = new RGB_Wrapper(Float.parseFloat(R),Float.parseFloat(G),Float.parseFloat(B));
+							rgb.setDate(SENT_TIME);
+							user_history.add(rgb);
+							
+						}
+						seekBar.setMax(user_history.size());
+						
+						Log.d("SIZE",String.valueOf(seekBar.getMax()));
+						
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+					}
+
+				} catch (ClientProtocolException e) {
+					// writing exception to log
+					e.printStackTrace();
+
+				} catch (IOException e) {
+					// writing exception to log
+					e.printStackTrace();
+				}
+			}
+		});
+		trd.start();
+	}
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
