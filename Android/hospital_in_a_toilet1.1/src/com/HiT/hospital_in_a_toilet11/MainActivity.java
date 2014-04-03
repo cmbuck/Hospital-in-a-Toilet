4d3b7317608e84267d9239152c5ef32ab3f44999
edougal <edougal@gmail.com>
2014-04-02 20:49:52 -0500

    added connect_to_server method

--- Android/hospital_in_a_toilet1.1/src/com/HiT/hospital_in_a_toilet11/MainActivity.java
+++ Android/hospital_in_a_toilet1.1/src/com/HiT/hospital_in_a_toilet11/MainActivity.java
@@ -1,6 +1,22 @@
 package com.HiT.hospital_in_a_toilet11;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Locale;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.HiT.hospital_in_a_toilet11.RGB_Wrapper;
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -9,8 +25,10 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SeekBar;
 
 public class MainActivity extends ActionBarActivity implements
 		ActionBar.TabListener {
@@ -29,6 +47,11 @@ public class MainActivity extends ActionBarActivity implements
 	 */
 	ViewPager mViewPager;
 
+    private ArrayList<RGB_Wrapper> user_history;
+    private static RGB_Wrapper last_collected;
+	private SeekBar seekBar;
+
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -160,6 +183,69 @@ public class MainActivity extends ActionBarActivity implements
 		}
 	}
 
+	public void connect_to_server(final String request, final boolean toAdd) {
+		Thread trd = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				
+				HttpClient httpClient = new DefaultHttpClient();
+				HttpGet httpGet = new HttpGet(request);
+
+				// Making HTTP Request
+				try {
+					HttpResponse response = httpClient.execute(httpGet);
+
+					// writing response to log
+					InputStream in = response.getEntity().getContent();
+					BufferedReader reader = new BufferedReader(
+							new InputStreamReader(in));
+					StringBuilder str = new StringBuilder();
+					String line = null;
+					while ((line = reader.readLine()) != null) {
+						str.append(line);
+					}
+					in.close();
+
+					Log.d("HTML", str.toString());
+					if(toAdd){
+					try {
+						final JSONObject json = new JSONObject(str.toString());
+						
+						for(Integer i=0;i<json.length();i++)
+						{
+							String res = json.getString(i.toString());
+							final JSONObject json_2 = new JSONObject(res);
+							String R = json_2.getString("R");
+							String G = json_2.getString("G");
+							String B = json_2.getString("B");
+							String SENT_TIME = json_2.getString("SENT_TIME");
+							Log.e("JSON", R + G + B +SENT_TIME);
+							RGB_Wrapper rgb = new RGB_Wrapper(Float.parseFloat(R),Float.parseFloat(G),Float.parseFloat(B));
+							rgb.setDate(SENT_TIME);
+							user_history.add(rgb);
+							
+						}
+						seekBar.setMax(user_history.size());
+						
+						Log.d("SIZE",String.valueOf(seekBar.getMax()));
+						
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+					}
+
+				} catch (ClientProtocolException e) {
+					// writing exception to log
+					e.printStackTrace();
+
+				} catch (IOException e) {
+					// writing exception to log
+					e.printStackTrace();
+				}
+			}
+		});
+		trd.start();
+	}
 	
 
 }
