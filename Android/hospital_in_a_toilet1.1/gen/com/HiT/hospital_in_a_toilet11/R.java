79ab84d95bc4b1babf60cb7600312f6421ec536a
Abhipray Sahoo <abhiprays@gmail.com>
2014-04-02 20:25:07 -0500

    Organized code and added jars
    -Added plotting library .jar
    -Split the Fragment classes into separate classes

--- Android/hospital_in_a_toilet1.1/gen/com/HiT/hospital_in_a_toilet11/R.java
+++ Android/hospital_in_a_toilet1.1/gen/com/HiT/hospital_in_a_toilet11/R.java
@@ -892,6 +892,13 @@ containing a value of this type.
          */
         public static final int activity_horizontal_margin=0x7f080011;
         public static final int activity_vertical_margin=0x7f080012;
+        public static final int domain_label_font_size=0x7f080015;
+        public static final int domain_tick_label_font_size=0x7f080018;
+        public static final int legend_text_font_size=0x7f080019;
+        public static final int pie_segment_label_font_size=0x7f080013;
+        public static final int range_label_font_size=0x7f080016;
+        public static final int range_tick_label_font_size=0x7f080017;
+        public static final int title_font_size=0x7f080014;
     }
     public static final class drawable {
         public static final int abc_ab_bottom_solid_dark_holo=0x7f020000;
@@ -995,7 +1002,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003e;
+        public static final int action_settings=0x7f05003f;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1017,6 +1024,7 @@ containing a value of this type.
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
+        public static final int mySimpleXYPlot=0x7f05003e;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
--- Android/hospital_in_a_toilet1.1/src/com/HiT/hospital_in_a_toilet11/MainActivity.java
+++ Android/hospital_in_a_toilet1.1/src/com/HiT/hospital_in_a_toilet11/MainActivity.java
@@ -1,7 +1,6 @@
 package com.HiT.hospital_in_a_toilet11;
 
 import java.util.Locale;
-
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -165,54 +164,6 @@ public class MainActivity extends ActionBarActivity implements
 		}
 	}
 
-	public static class TrendsSectionFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		public static final String ARG_SECTION_NUMBER = "section_number";
-
-		public TrendsSectionFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_trends,
-					container, false);
-			TextView dummyTextView = (TextView) rootView
-					.findViewById(R.id.section_label);
-			dummyTextView.setText(Integer.toString(getArguments().getInt(
-					ARG_SECTION_NUMBER)));
-			return rootView;
-		}
-	}
 	
-	/**
-	 * A dummy fragment representing a section of the app, but that simply
-	 * displays dummy text.
-	 */
-	public static class LiveSectionFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		public static final String ARG_SECTION_NUMBER = "section_number";
-
-		public LiveSectionFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_live,
-					container, false);
-			TextView dummyTextView = (TextView) rootView
-					.findViewById(R.id.section_label);
-			dummyTextView.setText(Integer.toString(getArguments().getInt(
-					ARG_SECTION_NUMBER)));
-			return rootView;
-		}
-	}
 
 }
